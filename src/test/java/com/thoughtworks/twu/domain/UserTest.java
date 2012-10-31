112046917546443103feb68a719013420b73b87b
Rajik <krajeswaribtech@gmail.com>
2012-10-31 13:38:30 +0530

    Update src/test/java/com/thoughtworks/twu/domain/UserTest.java

--- src/test/java/com/thoughtworks/twu/domain/UserTest.java
+++ src/test/java/com/thoughtworks/twu/domain/UserTest.java
@@ -10,30 +10,4 @@ import static org.junit.Assert.assertThat;
 
 public class UserTest
 {
-    private User user;
-    @Before
-    public void setUp(){
-    user = new User("Yue","yding@thoughtworks.com","123","13991871507");
-
-    }
-
-    @Test
-    public void testGetName()  {
-        assertThat(user.getName(),is("Yue"));
-
-    }
-
-    @Test
-    public void testGetEmail() {
-        assertThat(user.getEmail(),is("yding@thoughtworks.com"));
-    }
-    @Test
-    public void testGetPassword() {
-    assertThat(user.getPassword(),is("123"));
-    }
-
-    @Test
-    public void testGetPhoneNumber() {
-         assertThat(user.getPhoneNumber(),is("13991871507"));
-    }
-}
+ }
