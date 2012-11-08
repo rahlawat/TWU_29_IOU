a5fc58cba64246579423b544bf089809a35afd56
Sam Jacobs <email>
2012-11-08 10:47:26 +0530

    Story 145 (Renu/Sam) Added jasmine tests for recent features

--- src/main/webapp/jasmineTests/my-friends-listTest.js
+++ src/main/webapp/jasmineTests/my-friends-listTest.js
@@ -1,35 +1,32 @@
-describe("Email list table", function() {
+describe("Email list table", function () {
+    var input = $('<input type"text" id="newEmail">');
+    var emailTable = $(
 
-    beforeEach(function () {
-        var input = $('<input type"text" id="newEmail">');
-        var emailTable = $(
-
-            '<div id="emailListDiv">' +
-                '<table id="emailList" border="2">' +
-                '<tr style="background-color: #87cefa;">' +
-                    '<th id="header">Friends Added</th>' +
-                '</tr>' +
-                '<tr>' +
-                    '<td id="baseRow"> &nbsp; </td>' +
-                '</tr>' +
+        '<div id="emailListDiv">' +
+            '<table id="emailList" border="2">' +
+            '<tr style="background-color: #87cefa;">' +
+            '<th id="header">Friends Added</th>' +
+            '</tr>' +
+            '<tr>' +
+            '<td id="baseRow"> &nbsp; </td>' +
+            '</tr>' +
             '</table>' +
             '</div>');
-
-        $(document.body).append(input);
-        $(document.body).append(emailTable);
-
-
-    });
-
-    afterEach(function() {
-        $('#emailListDiv').remove();
-        $('#newEmail').remove();
-    });
-
 
     var row0;
     var row1;
     var row2;
+
+    beforeEach(function () {
+        $(document.body).append(input);
+        $(document.body).append(emailTable);
+    });
+
+    afterEach(function () {
+        $('#emailListDiv').remove();
+        $('#newEmail').remove();
+        rowCounter = 1;
+    });
 
     it("should be updated below Header when table empty", function () {
         document.getElementById('newEmail').value = "abc@gmail.com";
@@ -46,8 +43,7 @@ describe("Email list table", function() {
     });
 
 
-
-    it("should be updated at the top", function() {
+    it("should be updated at the top", function () {
         document.getElementById('newEmail').value = "abc@gmail.com";
 
         addToList();
@@ -67,7 +63,7 @@ describe("Email list table", function() {
 
     });
 
-    it("should clear the table when click save", function(){
+    it("should clear the table when click save", function () {
         document.getElementById('newEmail').value = "abc@gmail.com";
 
         addToList();
@@ -80,12 +76,65 @@ describe("Email list table", function() {
 
         var table = document.getElementById('emailList');
         row0 = table.rows[0].innerText;
-        row2 = table.rows[1].innerText;
+        row1 = table.rows[1].innerText;
         //row1 = table.rows[2];
 
         expect(row0).toMatch("Friends Added");
-        expect(row2).toMatch(String.fromCharCode(160));  //(&nbsp;)
+        expect(row1).toMatch(String.fromCharCode(160));  //(&nbsp;)
         //expect(row1).toMatch(null);
 
-    })
+    });
+
+    it("should clear input on save", function () {
+        document.getElementById('newEmail').value = "abc@gmail.com";
+        save();
+
+        expect(document.getElementById('newEmail').value).toBe("");
+    });
+
+    it("should clear input on add to list", function () {
+        document.getElementById('newEmail').value = "abc@gmail.com";
+        addToList();
+
+        expect(document.getElementById('newEmail').value).toBe("");
+    });
+
+    it("should reset the row counter on clearTable", function () {
+        document.getElementById('newEmail').value = "abc@gmail.com";
+        addToList();
+        document.getElementById('newEmail').value = "xyz@gmail.com";
+        addToList();
+
+        expect(rowCounter).toEqual(3);
+
+        clearTable();
+
+        expect(rowCounter).toEqual(1);
+
+    });
+
+    it("should not add to table if input empty", function () {
+        document.getElementById('newEmail').value = "";
+        var tableBefore = document.getElementById('emailList')
+        addToList();
+
+        var tableAfter = document.getElementById('emailList');
+
+
+        expect(tableAfter).toEqual(tableBefore);
+
+
+
+
+    });
+
+    it("should not increment rowCounter on add if input empty", function () {
+        document.getElementById('newEmail').value = "";
+        addToList();
+
+        expect(rowCounter).toEqual(1);
+
+
+    });
+
 });
