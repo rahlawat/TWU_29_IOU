3266c1a0a2b663e911870a9987825918390c6f94
Sam Jacobs <email>
2012-11-09 10:28:22 +0530

    Story 145 Forgot to actually add Yue, now she is

--- src/main/resources/twu_database/import.sql
+++ src/main/resources/twu_database/import.sql
@@ -3,3 +3,4 @@ use IOUdb;
 
 insert into UserTable(email,username,password,phoneNumber) values('sajacobs@thoughtworks.com','Sam Jacobs','1234','999-9999');
 insert into UserTable(email,username,password,phoneNumber) values('rahlawat@thoughtworks.com','Renu Ahlawat','1234','999-9999');
+insert into UserTable(email,username,password,phoneNumber) values('yding@thoughtworks.com', 'Yue', 'yue123','13991871507');
